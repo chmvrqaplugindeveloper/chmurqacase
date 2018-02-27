57259ac59bed8d41c67bbc604c798b4106f4b9d8
chmvrqaplugindeveloper <36896699+chmvrqaplugindeveloper@users.noreply.github.com>
2018-02-27 19:49:24 +0100

    added config
    a config haven't any text

--- chmurqacase/src/pl/itzchmurqa/chmurqacase/Main.java
+++ chmurqacase/src/pl/itzchmurqa/chmurqacase/Main.java
@@ -1,5 +1,12 @@
 package pl.itzchmurqa.chmurqacase;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+ 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,16 +15,31 @@ import pl.itzchmurqa.chmurqacase.commands.SkrzynkaCommand;
 import pl.itzchmurqa.chmurqacase.listener.BlockPlaceListener;
 
 public class Main extends JavaPlugin{
-
+	File newConfig;
+	FileConfiguration newConfigz;
 	private static Main i;
 	
 	public void onEnable(){
+		newConfig = new File(getDataFolder(), "config.yml");
+		newConfigz = YamlConfiguration.loadConfiguration(newConfig);
+		saveNewConfig();
 		i = this;
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new BlockPlaceListener(), this);
 		getCommand("skrzynka").setExecutor(new SkrzynkaCommand());
 	}
+	public void saveNewConfig(){
+		try{
+		newConfigz.save(newConfig);
+		 
+		}catch(Exception e){
+		e.printStackTrace();
+		}
+	}
 	
+	public void onDisable(){
+		 
+	}
 	
 	
 	public static Main getInstance() {
